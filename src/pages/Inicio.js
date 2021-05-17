2cbe1c69e4c33d72d427c57265d6132c8dbbf5e8
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-17 13:34:30 -0300

    OK: como conseguir login

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -17,6 +17,13 @@ Todavía la estamos inventando. ¿Te gustaría ayudarnos y aparecer en los créd
 * Dibujando pantallas, haciendo experimientos, investigando otras apps con la #bandaUX_UI
 * Simplemente contando qué te gustaría y como te fue
 
+Puede que algunas opciones del menú te pidan usuario y clave, si todavía no los creaste
+
+1. Ingresá a https://si.podemosaprender.org
+2. Registrate con Facebook o Gmail
+3. Aprovecha y asociá todas tus cuentas
+4. Agregale una clave a tu cuenta
+
 		`}
 		</MarkdownMostrar>
 	)
