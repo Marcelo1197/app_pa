2f10e4419d31d64f26e3a62c155438b22532d975
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-17 06:03:56 -0300

    OK: filtros para textos

---
+++ src/components/TextoFiltros.js
@@ -0,0 +1,38 @@
+//INFO: editar los filtros aplicados a textos
+
+import { fechaLegible } from '../services/pa-lib';
+
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CharlaIcon from '@material-ui/icons/Chat';
+import FHMaxIcon from '@material-ui/icons/Schedule';
+import DeIcon from '@material-ui/icons/Person';
+
+export default function TextoFiltros({filtros, setFiltros}) {
+	const cuandoPideBorrar= (k) => {
+		setFiltros({ ...filtros, [k]: null });
+	}
+
+	return (
+		<div>
+			{ Object.entries(filtros).map( ([k,v]) => {
+				let txt= k; let icon=null;
+
+				//TODO: se puede generalizar para cualquier set de filtros
+				if (k=='fh_max') { txt= fechaLegible(v); icon= <FHMaxIcon />; }
+				else if (k=='charla') { txt= v; icon= <CharlaIcon />; }
+				else if (k=='de') { txt=v; icon= <DeIcon />; }
+
+				return (
+					<Button
+						variant="contained"
+						startIcon={icon}
+						endIcon={<DeleteIcon />}
+						onClick={() => cuandoPideBorrar(k)}
+					>{txt}</Button>
+				)
+			})}
+		</div>
+	)
+}
+
--- src/pages/Textos.jsx
+++ src/pages/Textos.jsx
@@ -1,17 +1,20 @@
 //INFO: mostrar lista de textos con filtros y orden
 
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useUrlSearchParams } from '../hooks/useUrlSearchParams';
 import { urlParamsParaDiccionario } from '../services/pa-lib';
 import { fechaLegible, fechasSonIguales, fechaParaTexto } from '../services/pa-lib';
 import { usePaApiConsulta } from '../hooks/usePaApiConsulta';
 
+import TextoFiltros from '../components/TextoFiltros';
 import MarkdownMostrar from '../components/MarkdownMostrar';
 
 import {Link} from "react-router-dom";
 import Button from '@material-ui/core/Button';
 
 export default function Texto() {
+	const history= useHistory();
 	const urlSearchParams= useUrlSearchParams(); //A: ej fh_max=2021-05-12
 	const [textos, filtros, setFiltros]= usePaApiConsulta();
 
@@ -28,11 +31,21 @@ export default function Texto() {
 	const fh_min= new Date(textos.reduce((acc, t) => Math.min(acc, t.fhCreado), new Date()));
 	//DBG: console.log("Textos fh_min fh_max",fh_min, fh_max_proxima)
 
+	const cuandoCambiaFiltros= (filtros) => {
+		history.push('/textos/?'+urlParamsParaDiccionario(filtros)); //A: navegamos a nueva url
+	};
+
 	return (
 		<>
 			<h2>
 				Textos {filtros.fhEditado_Lt ? `(desde ${fechaLegible(filtros.fhEditado_Lt)})`: ''}
 			</h2>
+
+			<TextoFiltros 
+				filtros={urlSearchParams} 
+				setFiltros={cuandoCambiaFiltros}
+			/>
+
 			{ textos 
 				? (<> 
 						<>
@@ -42,7 +55,7 @@ export default function Texto() {
 									to={{pathname: `/texto/${texto.textoId}`, state: texto}}
 								>{fechaLegible( texto.fhCreado )}
 								</Link> por <Link
-									to={{ search: '?'+urlParamsParaDiccionario(Object.assign({},urlSearchParams,{de:texto.deQuien})), state: texto}}
+									to={{ search: '?'+urlParamsParaDiccionario({ ...urlSearchParams, de:texto.deQuien}), state: texto}}
 								>{texto.deQuien}
 								</Link>
 								<MarkdownMostrar 
@@ -56,7 +69,7 @@ export default function Texto() {
 						}
 						</>
 						<Button 
-							to={{search: '?'+urlParamsParaDiccionario(Object.assign({},urlSearchParams,{fh_max:fh_min}))}}
+							to={{search: '?'+urlParamsParaDiccionario({ ...urlSearchParams, fh_max:fh_min})}}
 							component={Link}
 						>Más viejos</Button>	
 					</>)
