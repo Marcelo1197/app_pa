8949d12e8cd3f905812a6a23ae950edddd926e33
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-18 21:02:45 -0300

    OK: Chips para los filtros

--- src/components/TextoFiltros.js
+++ src/components/TextoFiltros.js
@@ -2,19 +2,36 @@
 
 import { fechaLegible } from '../services/pa-lib';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import Button from '@material-ui/core/Button';
+//VER: https://material-ui.com/components/chips/
+import Chip from '@material-ui/core/Chip';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CharlaIcon from '@material-ui/icons/Chat';
 import FHMaxIcon from '@material-ui/icons/Schedule';
 import DeIcon from '@material-ui/icons/Person';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		justifyContent: 'left',
+		flexWrap: 'wrap',
+		'& > *': {
+			margin: theme.spacing(0.5),
+		},
+	},
+}));
+
 export default function TextoFiltros({filtros, setFiltros}) {
+	const classes = useStyles();
+
 	const cuandoPideBorrar= (k) => {
 		setFiltros({ ...filtros, [k]: null });
 	}
 
 	return (
-		<div>
+		<div className={classes.root}>	
 			{ Object.entries(filtros).map( ([k,v]) => {
 				let txt= k; let icon=null;
 
@@ -24,14 +41,12 @@ export default function TextoFiltros({filtros, setFiltros}) {
 				else if (k=='de') { txt=v; icon= <DeIcon />; }
 
 				return (
-					<Button
-						key={k}
-						startIcon={icon}
-						onClick={() => cuandoPideBorrar(k)}
-
-						endIcon={<DeleteIcon />}
-						variant="contained"
-					>{txt}</Button>
+					<Chip key={k}
+						onDelete={() => cuandoPideBorrar(k)}
+						label={txt}
+						icon={icon}
+					>
+					</Chip>
 				)
 			})}
 		</div>
