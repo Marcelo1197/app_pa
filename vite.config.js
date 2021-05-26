b2767241586ad680ab05278ffb148e7f5f6e403c
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-26 12:31:39 -0300

    FIX: puerto 8443 en glitch

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,45 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 //import eslint from 'vite-plugin-eslint';
 import fs from "fs";
 
+const HMR_PORT=process.env.PROJECT_REMIX_CHAIN ? 8443 : null; //A: en glitch usamos 8443
+console.log('HMR_PORT',HMR_PORT);
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	esbuild: { loader: "jsx", include: /src\/.*\.jsx?$/, exclude: [] }, 
 	optimizeDeps: { esbuildOptions: { plugins: [ { name: "load-js-files-as-jsx", setup(build) { build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => { return ({ loader: "jsx", contents: await fs.promises.readFile(args.path, "utf8"), }) }); }, }, ], }, },
 	plugins: [
 		reactRefresh(),
-/*		eslint({
-			cache: false,
-			fix: false,
-			include: ['./*.js', './*.jsx'],
-		}),
-*/
 	],
 	build: {
 		outDir: "build"
 	},
 	server: {
 		strictPort: true,
-			hmr: {
-				//port: 8443 // Run the websocket server on the SSL port
-			}
+		hmr: {
+			port: HMR_PORT,
+		}
+	}
+});
+export default defineConfig({
+	esbuild: { loader: "jsx", include: /src\/.*\.jsx?$/, exclude: [] }, 
+	optimizeDeps: { esbuildOptions: { plugins: [ { name: "load-js-files-as-jsx", setup(build) { build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => { return ({ loader: "jsx", contents: await fs.promises.readFile(args.path, "utf8"), }) }); }, }, ], }, },
+	plugins: [
+		reactRefresh(),
+		/*		eslint({
+			cache: false,
+			fix: false,
+			include: ['./*.js', './*.jsx'],
+		}),
+		*/
+	],
+	build: {
+		outDir: "build"
+	},
+	server: {
+		strictPort: true,
+		hmr: {
+			//port: 8443 // Run the websocket server on the SSL port
+		}
 	}
 });
